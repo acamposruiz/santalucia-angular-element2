a2090906d8b47f94d9b4593012b18a23498ca9e2
Antonio <acamposruiz@gmail.com>
2021-10-20 15:38:21 +0200

    feat: general rules

--- src/santalucia-angular-element/index.ts
+++ src/santalucia-angular-element/index.ts
@@ -1,11 +1,24 @@
-import { Rule, SchematicContext, Tree } from "@angular-devkit/schematics";
+import {
+  Rule,
+  SchematicContext,
+  Tree,
+  externalSchematic,
+} from "@angular-devkit/schematics";
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
 export function santaluciaAngularElement(_options: any): Rule {
   const name = _options.name;
   console.log("The name of the repo will be", name);
-  return (tree: Tree, _context: SchematicContext) => {
-    return tree;
+  return (_: Tree, _context: SchematicContext) => {
+    return externalSchematic("@schematics/angular", "ng-new", {
+      name,
+      version: "9.0.0",
+      directory: name,
+      routing: false,
+      style: "scss",
+      inlineStyle: false,
+      inlineTemplate: false,
+    });
   };
 }
